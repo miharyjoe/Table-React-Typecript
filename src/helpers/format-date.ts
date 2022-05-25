8c1db468e17e670c35f93bcf5837c34b56a9b51c
Mihary Joel ANDRIAMILANTO <hei.mihary.2@gmail.com>
2022-05-25 23:52:24 +0300

    uptade

--- src/helpers/format-date.ts
+++ src/helpers/format-date.ts
@@ -1,5 +1,6 @@
 const formatDate = (date: Date): string => {
-    return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+    let month = date.getMonth()+1;
+    return `${date.getDate().toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${date.getFullYear()}`;
 }
 
 export default formatDate;
